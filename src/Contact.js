41fca1bcae6e7303c2b6d229dfe38a2ae62e4f8a
Mac <xuromeo@gmail.com>
2020-07-19 21:34:48 -0700

    update

--- src/Contact.js
+++ src/Contact.js
@@ -1,13 +1,57 @@
 import React from "react";
-export default function Contact() {
-  return (
-    <div>
-      <h2>contact form </h2>
-      <form className="form" data-netlify="true">
-        <input className="input" placeholder="name" type="text" />
-        <input className="input" placeholder="email" type="text" />
-        <textarea className="input" placeholder="your request" type="text" />
-        <button>Submit</button>
-      </form>
-    </div>)
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
 }
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+  /* Here’s the juicy bit for posting the form submission */
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, message } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message" value={message} onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    );
+  }
+}
+
+export default Contact;
