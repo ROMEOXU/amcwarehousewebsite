09a0e3d0adf2d8a5f9687e2f62285caa25514b4a
Mac <xuromeo@gmail.com>
2020-08-27 22:05:27 -0700

    update contact

--- src/Contact.js
+++ src/Contact.js
@@ -31,8 +31,15 @@ class Contact extends React.Component {
     const { name, email, message } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
-        <h3>info@amcwarehouse.com</h3>
-        <p>
+        <br/>
+        <br/>
+        <br/>
+        <h3>welcome to contact our email: AMCwarehouseteam@gmail.com</h3>
+        <h3>Address:1946 Sabre St , Haywared ,CA</h3>
+        <p>Alan 510-356-7190</p><p>LuLu 650-933-6166</p>
+        
+        <p>Romeo 510-509-5811</p>
+        {/* <p>
           <label>
             Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
           </label>
@@ -49,7 +56,7 @@ class Contact extends React.Component {
         </p>
         <p>
           <button type="submit">Send</button>
-        </p>
+        </p> */}
       </form>
     );
   }
