d0d50f13fcbdc77ce458fd9452ae64abe4f77bb2
Mac <xuromeo@gmail.com>
2020-09-12 17:18:23 -0700

    pic

--- src/Service.js
+++ src/Service.js
@@ -1,5 +1,6 @@
 import React from "react";
-import warehpusepic from './pic/warehouse.jpg'
+import warehpusepic from './pic/IMG_6421.JPG';
+import pallet from './pic/IMG_6423.JPG';
 export default function Service() {
   return (
     <div>
@@ -9,6 +10,7 @@ export default function Service() {
         price <br /> and have been doing port drayage transportation since 2016.
       </p>
       <img className = 'warehousePic' src={warehpusepic} alt='warehouse' />
+      <img className = 'warehousePic' src={pallet} alt='warehouse' />
     </div>
   );
 }
