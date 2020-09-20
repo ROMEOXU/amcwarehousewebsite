a782fc32481302b3a388707e6494e6a67d91a3a6
Mac <xuromeo@gmail.com>
2020-09-19 17:42:33 -0700

    service

--- src/Service.js
+++ src/Service.js
@@ -4,11 +4,14 @@ import pallet from './pic/IMG_6423.JPG';
 export default function Service() {
   return (
     <div>
-      <h2> </h2>
+      <h2>Prep</h2>
       <p>
-        We provide the high qulaity transload warehouse service with competitive
-        price <br /> and have been doing port drayage transportation since 2016.
+      Amazon has packaging and prep requirements for products being shipped and stored in FBA centers. Properly packaging and preparing units helps to reduce delays in getting your products in. We are experts in Amazon-compliance, ensuring that your products will not be rejected.
       </p>
+      <h2>Storage</h2>
+      <p>While most companies, including Amazon have ever-changing charges for FBA storage, We offers a flat rate. No hidden fees and no long-term charges.</p>
+      <h2>Drayage</h2>
+      <p>Let us handle your Amazon FCL Drayage transportation needs from Oakland Port. If the destination is not local, we will assist with transloading, which is common for Amazon shipments.</p>
       <img className = 'warehousePic' src={warehpusepic} alt='warehouse' />
       <img className = 'warehousePic' src={pallet} alt='warehouse' />
     </div>
