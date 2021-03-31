38d11ac4bbab9175a0d80a48481db1a6b30b0f0a
Mac <xuromeo@gmail.com>
2021-03-30 20:28:34 -0700

    repsonsive pic

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,9 @@ import homepic from "./pic/IMG_6442.JPG";
 export default function Home() {
   return (
     <div className = 'cargopic-div'>
+      <img className="cargopic shen" src={homepic} alt="home" />
       <img className="cargopic" src={homepic} alt="home" />
-      <img className="cargopic" src={homepic} alt="home" />
-      <img className="cargopic" src={homepic} alt="home" />
+      <img className="cargopic shen" src={homepic} alt="home" />
     </div>
   );
 }
